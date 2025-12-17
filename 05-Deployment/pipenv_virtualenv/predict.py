f51b353c73b0a2d8bb34cb7fb734bf69f15df659
bjdeb1986 <64271803+bjdeb1986@users.noreply.github.com>
2025-12-17 18:40:48 +0000

    pipenv virtual env to run any py code with specific import package versions. this is nothing but virtual env which can have it's own packages to run code

---
+++ 05-Deployment/pipenv_virtualenv/predict.py
@@ -0,0 +1,40 @@
+# we can run this file inside pipenv virtual env using any of the following command
+# Approach 1:
+#   pipenv shell
+#   gunicorn --bind 0.0.0.0:9696 predict:app
+# Approach 2:
+#   pipenv run gunicorn --bind 0.0.0.0:9696 predict:app
+
+import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+
+model_file = 'model_C=1.0.bin'
+with open(model_file, 'rb') as f_in: 
+    dv, model = pickle.load(f_in)
+# dv, model
+
+app = Flask('churn')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
+
+# print(f'Input: {customer}')
+# print(f'Churn probability: {y_pred}')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
